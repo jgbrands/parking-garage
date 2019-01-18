85a59773e0fd1e862732fe78f1c3a3f2858c57f6
elbble123 <e.l.boerema@st.hanze.nl>
2019-01-18 17:02:31 +0100

    created the application class

--- src/main/java/nl/cityparking/garfield/ParkingGarage.java
+++ src/main/java/nl/cityparking/garfield/ParkingGarage.java
@@ -6,13 +6,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import nl.cityparking.garfield.gui.PrimaryView;
+import nl.cityparking.garfield.gui.simulator.ParkingLot;
+import nl.cityparking.garfield.simulator.ParkingSpace;
 import nl.cityparking.garfield.simulator.Simulator;
 import nl.cityparking.garfield.simulator.config.Configuration;
 import nl.cityparking.garfield.simulator.config.SpawnRatio;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class ParkingGarage extends Application {
+	private PrimaryView primaryViewController;
+	private Pane primaryView;
+	private Pane parkingLotView;
+	private Pane distributionChartView;
+
 	public static void main(String[] args) {
 		Configuration configuration = new Configuration();
 
@@ -42,18 +51,44 @@ public class ParkingGarage extends Application {
 	public void start(Stage stage) {
 		stage.setTitle("Parking Garage");
 
-		// Load our views viewer.
 		try {
-			URL resource = this.getClass().getResource("/views/distributionChart.fxml");
-			if (resource != null) {
-				FXMLLoader loader = new FXMLLoader();
-
-				Pane root = loader.load(resource.openStream());
-				stage.setScene(new Scene(root, 0, 0));
-				stage.show();
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			FXMLLoader loader = this.createLoader("/views/primaryView.fxml");
+			this.primaryView = loader.load();
+			this.primaryViewController = loader.getController();
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+
+		if (this.primaryView != null) {
+			stage.setScene(new Scene(this.primaryView, 0, 0));
+			stage.show();
+
+			try {
+				FXMLLoader loader = this.createLoader("/views/parkingLot.fxml");
+				this.parkingLotView = loader.load();
+				this.primaryViewController.setMainView(this.parkingLotView);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			try {
+				FXMLLoader loader = this.createLoader("/views/distributionChart.fxml");
+				this.distributionChartView = loader.load();
+				this.primaryViewController.setSecondView(this.distributionChartView);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public FXMLLoader createLoader(String filename) {
+		URL resource = this.getClass().getResource(filename);
+
+		if (resource != null) {
+			FXMLLoader loader = new FXMLLoader(resource);
+			return loader;
+		}
+
+		return null;
 	}
 }
